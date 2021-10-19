535003a5a292517812117d2116662a442555e826
RomanVoznyy <roman.voznyy.52@gmail.com>
2021-10-19 23:39:12 +0300

    finished task2 phonebook

--- src/App.js
+++ src/App.js
@@ -1,53 +1,44 @@
 import { useState } from 'react';
-import { nanoid } from 'nanoid';
+import AddForm from './Components/AddForm';
+import Contacts from './Components/Contacts';
+import Filter from './Components/Filter';
 import './App.css';
 
 const INITIAL_STATE = {
-  contacts: [],
-  name: '',
+  contacts: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
+  filter: '',
 };
 
 function App() {
   const [contacts, setContacts] = useState(INITIAL_STATE.contacts);
-  const [name, setName] = useState(INITIAL_STATE.name);
+  const [filter, setFilter] = useState(INITIAL_STATE.filter);
 
-  const handleName = ({ target }) => {
-    setName(target.value);
-  };
+  const handleInputField = ({ target }) => setFilter(target.value);
 
-  const handleSubmit = evt => {
-    evt.preventDefault();
-    setContacts(prevState => [...prevState, { name, id: nanoid() }]);
+  const filteredArray = arr =>
+    arr.filter(el => el.name.toLowerCase().includes(filter.toLowerCase()));
+
+  const deleteContact = id => {
+    setContacts(prevState => prevState.filter(el => el.id !== id));
   };
 
   return (
     <div className="App">
       <h1>PhoneBook</h1>
-      <form className="form" onSubmit={handleSubmit}>
-        <label className="labelNameField">
-          <span className="titleNameField">Name</span>
-          <input
-            type="text"
-            className="nameField"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов."
-            value={name}
-            onChange={handleName}
-            required
-          />
-        </label>
-        <button type="submit">Add contact</button>
-      </form>
-      <h1>Contacts</h1>
-      <ul>
-        {contacts.length > 0 &&
-          contacts.map(el => (
-            <li key={el.id} className="listItem">
-              {el.name}
-            </li>
-          ))}
-      </ul>
+      <h3>Add new contact</h3>
+      <AddForm contacts={contacts} setContacts={setContacts} />
+      <h2>Contacts</h2>
+      <Filter filter={filter} handleInputField={handleInputField} />
+      <Contacts
+        contacts={contacts}
+        filteredArray={filteredArray}
+        deleteContact={deleteContact}
+      />
     </div>
   );
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+arr = ['apple', 'orange', 'plum', 'banana'];
+text = 'pl';
+console.log(arr[1].includes(text));
